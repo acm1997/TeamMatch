9bbd190d8325a89f4ecac158c9b38b4e09151fe7
Alberto <aayusoma@alumnos.unex.es>
2020-11-08 19:15:25 +0100

    Commit crear evento tiempo y hora

--- app/src/main/java/com/example/teammatch/activities/CrearEventoActivity.java
+++ app/src/main/java/com/example/teammatch/activities/CrearEventoActivity.java
@@ -1,12 +1,19 @@
 package com.example.teammatch.activities;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,9 +21,20 @@ import com.example.teammatch.R;
 import com.example.teammatch.objects.Evento;
 import com.example.teammatch.objects.Evento.Deporte;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class CrearEventoActivity extends AppCompatActivity {
+
+    // 7 days in milliseconds - 7 * 24 * 60 * 60 * 1000
+    private static final int SEVEN_DAYS = 604800000;
+
+    private static final String TAG = "Lab-UserInterface";
+
+    private static String horaString;
+    private static String fechaString;
+    private static TextView fechaView;
+    private static TextView horaView;
 
     private EditText mNombre;
     private Date mFecha;
@@ -31,11 +49,41 @@ public class CrearEventoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nuevo_evento);
-      //  TextView tVSaludo = findViewById(R.id.tVSaludo);
+        mNombre = (EditText) findViewById(R.id.nombreEvento);
+        mParticipantes =(EditText) findViewById(R.id.numParticipantes);
+        mDescripcion = (EditText) findViewById(R.id.descEvento);
+        mDeportes = (RadioGroup) findViewById(R.id.radioGroup);
+        mDefaultDeporte = (RadioButton) findViewById(R.id.radioFutbol);
+        fechaView = (TextView) findViewById(R.id.fecha);
+        horaView = (TextView) findViewById(R.id.hora);
 
-        final EditText tVNombreEvento = findViewById(R.id.nombreEvent);
-        final EditText tVFechaEvento = findViewById(R.id.fechaEvento);
+        // Set the default date and time
 
+        setDefaultDateTime();
+
+        // OnClickListener for the Date button, calls showDatePickerDialog() to show
+        // the Date dialog
+
+        final Button datePickerButton = (Button) findViewById(R.id.botonfecha);
+        datePickerButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+            }
+        });
+
+        // OnClickListener for the Time button, calls showTimePickerDialog() to show
+        // the Time Dialog
+
+        final Button timePickerButton = (Button) findViewById(R.id.botonhora);
+        timePickerButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                showTimePickerDialog();
+            }
+        });
 
 
         final Button cancelButton = (Button) findViewById(R.id.cancelButton);
@@ -47,14 +95,6 @@ public class CrearEventoActivity extends AppCompatActivity {
             }
         });
 
-        final Button botonBuscar = findViewById(R.id.idBotonBuscar);
-        botonBuscar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent2 = new Intent(CrearEventoActivity.this, buscarActivity.class); //MainActivity.this es el MainActivity,IMPORTANTE porque en este caso si ponemos solo this se refiere a la clase creada OneClickListener
-                startActivityForResult(intent2, BUSCAR_EVENTO_REQUEST);
-            }
-        });
 
         final  Button submitButton = findViewById(R.id.buttonCrear);
         submitButton.setOnClickListener(new View.OnClickListener() {
@@ -77,10 +117,123 @@ public class CrearEventoActivity extends AppCompatActivity {
 
     }
 
+    private void setDefaultDateTime() {
+
+        // Default is current time + 7 days
+        mFecha = new Date();
+        mFecha = new Date(mFecha.getTime() + SEVEN_DAYS);
+
+        Calendar c = Calendar.getInstance();
+        c.setTime(mFecha);
+
+        setDateString(c.get(Calendar.YEAR), c.get(Calendar.MONTH),
+                c.get(Calendar.DAY_OF_MONTH));
+
+        fechaView.setText(fechaString);
+
+        setTimeString(c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE),
+                c.get(Calendar.MILLISECOND));
+
+        horaView.setText(horaString);
+    }
+
+    private static void setDateString(int year, int monthOfYear, int dayOfMonth) {
+
+        // Increment monthOfYear for Calendar/Date -> Time Format setting
+        monthOfYear++;
+        String mon = "" + monthOfYear;
+        String day = "" + dayOfMonth;
+
+        if (monthOfYear < 10)
+            mon = "0" + monthOfYear;
+        if (dayOfMonth < 10)
+            day = "0" + dayOfMonth;
+
+        fechaString = year + "-" + mon + "-" + day;
+    }
+
+    private static void setTimeString(int hourOfDay, int minute, int mili) {
+        String hour = "" + hourOfDay;
+        String min = "" + minute;
+
+        if (hourOfDay < 10)
+            hour = "0" + hourOfDay;
+        if (minute < 10)
+            min = "0" + minute;
+
+        horaString = hour + ":" + min + ":00";
+    }
+
+    private void showDatePickerDialog() {
+        DatePickerFragment dpf = new DatePickerFragment();
+        dpf.show(getFragmentManager(),"dpf");
+
+    }
+
+    private void showTimePickerDialog() {
+        TimePickerFragment tpf = new TimePickerFragment();
+        tpf.show(getFragmentManager(),"tpf");
+
+
+    }
+
+    // DialogFragment used to pick a ToDoItem deadline date
+
+    public static class DatePickerFragment extends DialogFragment implements
+            DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+            // Use the current date as the default date in the picker
+
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            setDateString(year, monthOfYear, dayOfMonth);
+
+            fechaView.setText(fechaString);
+        }
+
+    }
+
+    // DialogFragment used to pick a ToDoItem deadline time
+
+    public static class TimePickerFragment extends DialogFragment implements
+            TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    true);
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            setTimeString(hourOfDay, minute, 0);
+
+            horaView.setText(horaString);
+        }
+    }
     private Deporte getDeporte() {
 
         switch (mDeportes.getCheckedRadioButtonId()) {
-            case R.id.radioFutboll: {
+            case R.id.radioFutbol: {
                 return Deporte.FUTBOL;
             }
             case R.id.radioBaloncesto: {
